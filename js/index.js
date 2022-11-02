480c02399355667306acf7fc397b089eab14df83
heejiyang <dianoheeji@naver.com>
2022-11-02 22:48:45 +0900

    add: CSS, HTML

--- js/index.js
+++ js/index.js
@@ -1,17 +1,50 @@
-const itemList = document.querySelectorAll('.cola_type > li');
-const getItemList = document.querySelector(".get_drink");
+const itemList = document.querySelectorAll(".item-list > figure");
+const getItemList = document.querySelector(".btn-get");
+const cartItems = [];
+console.log(cartItems);
 
-const setCartItem = (item) => {
-    const src = item.querySelector('img').getAttribute("src");
-    console.log(src);
-    // const cloneLi = item.cloneNode(true);
-    // console.log(cloneLi);
-    // getItemList.append
+
+
+const setCartItem = (item, id) => {
+    const src = item.querySelector("img").getAttribute("src");
+    const title = item.querySelector(".txt-item").textContent;
+    const findItemIdx = cartItems.findIndex((el) => el.id === id);
+    // 일치하는 것이 없으면 -1을 반환한다.
+    let quantity = 1;
+
+    // 해당하는 요소가 있을 경우(선택한것 또 선택한 경우)->수량늘린다.
+    if (findItemIdx !== -1) {
+        if(cartItems[findItemIdx].quantity >= 5) {
+            item.classList.add("soldout");
+            return;
+        }
+        return cartItems(findItemIdx).quantity++;
+    }
+    // 해당요소 없는 경우 push된다.
+    return cartItems.push({id, src, title, quantity});
 }
 
-itemList.forEach(item => {
+const renderCartItem = () => {
+    getItemList.innerHTML = "";
+
+    cartItems.forEach((item) => {
+        getItemList.innerHTML +=
+        `
+        <figure>
+            <img src = ${item.src} >
+            <p>${item.title}</p>
+            <span>${item.quantity}</span>
+        </figure>
+        `;
+    });
+};
+
+itemList.forEach((item) => {
+    const id = 1;
+
     item.addEventListener('click', () => {
         item.classList.add("active");
-        setCartItem(item);
+        setCartItem(item, id);
+        renderCartItem();
     });
 })
