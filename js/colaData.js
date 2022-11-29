c50ddce5b809a3b49fcf2f93ddf0bb53c968402f
heejiyang <dianoheeji@naver.com>
2022-11-30 03:46:09 +0900

    js colaList

---
+++ js/colaData.js
@@ -0,0 +1,40 @@
+const colaData = [
+    {
+        name: 'Original_Cola',
+        img: '../img/Original_Cola.svg',
+        price: 1000,
+        stock: 5,
+    },
+    {
+        name: 'Violet_Cola',
+        img: '../img/Violet_Cola.svg',
+        price: 1000,
+        stock: 5,
+    },
+    {
+        name: 'Yellow_Cola',
+        img: '../img/Yellow_Cola.svg',
+        price: 1000,
+        stock: 5,
+    },
+    {
+        name: 'Cool_Cola',
+        img: '../img/Cool_Cola.svg',
+        price: 1000,
+        stock: 5,
+    },
+    {
+        name: 'Green_Cola',
+        img: '../img/Green_Cola.svg',
+        price: 1000,
+        stock: 5,
+    },
+    {
+        name: 'Orange_Cola',
+        img: '../img/Orange_Cola.svg',
+        price: 1000,
+        stock: 5,
+    },
+]
+
+export default colaData;
--- js/index.js
+++ js/index.js
@@ -1,48 +1,23 @@
-const itemList = document.querySelectorAll(".item-list > figure");
-const getItemList = document.querySelector(".btn-get");
-const cartItems = [];
-console.log(cartItems);
+import colaData from './colaData.js'
+console.log(colaData)
 
-const setCartItem = (item, id) => {
-    const src = item.querySelector("img").getAttribute("src");
-    const title = item.querySelector(".txt-item").textContent;
-    const findItemIdx = cartItems.findIndex((el) => el.id === id);
-    // 일치하는 것이 없으면 -1을 반환한다.
-    let quantity = 1;
+const colaList = document.querySelector('.list-item');
+const inpDeposit = document.querySelector('#money');
+const btnDeposit = document.querySelector('.btn-put-money');
+const remain = document.querySelector('.remain');
+const btnReturn = document.querySelector('.btn-return');
 
-    // 해당하는 요소가 있을 경우(선택한것 또 선택한 경우)->수량늘린다.
-    if (findItemIdx !== -1) {
-        if(cartItems[findItemIdx].quantity >= 5) {
-            item.classList.add("soldout");
-            return;
-        }
-        return cartItems(findItemIdx).quantity++;
-    }
-    // 해당요소 없는 경우 push된다.
-    return cartItems.push({id, src, title, quantity});
-}
+const btnGet = document.querySelector('.btn-pocket');
+const cart = document.querySelector('.get-list');
 
-const renderCartItem = () => {
-    getItemList.innerHTML = "";
-
-    cartItems.forEach((item) => {
-        getItemList.innerHTML +=
-        `
-        <figure>
-            <img src = ${item.src} >
-            <p>${item.title}</p>
-            <span>${item.quantity}</span>
-        </figure>
-        `;
-    });
-};
-
-itemList.forEach((item) => {
-    const id = 1;
-
-    item.addEventListener('click', () => {
-        item.classList.add("active");
-        setCartItem(item, id);
-        renderCartItem();
-    });
+colaData.forEach((item) => {
+  const listItem = document.createElement('li');
+  listItem.innerHTML = `
+    <button class="item" type="button" data-item="${item.name}" data-stock="${item.stock}" data-img="${item.img}" data-price="${item.price}">
+      <img src="./img/${item.img}" alt="${item.name}" class="img-item">
+      <p class="txt-item">${item.name}</p>
+      <p class="price-item">${item.price}원</p>
+    </button>
+  `
+  colaList.appendChild(listItem);
 })
