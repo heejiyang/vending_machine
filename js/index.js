22836891abf02745b4a7ff388d20947899183951
heejiyang <dianoheeji@naver.com>
2022-12-02 02:23:24 +0900

    js 수정

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import colaData from './colaData.js'
-console.log(colaData)
+// console.log(colaData)
 
 const colaList = document.querySelector('.list-item');
 const inpDeposit = document.querySelector('#money');
@@ -10,6 +10,7 @@ const btnReturn = document.querySelector('.btn-return');
 const btnGet = document.querySelector('.btn-pocket');
 const cart = document.querySelector('.get-list');
 
+// 콜라 종류별 버튼
 colaData.forEach((item) => {
   const listItem = document.createElement('li');
   listItem.innerHTML = `
@@ -21,3 +22,14 @@ colaData.forEach((item) => {
   `
   colaList.appendChild(listItem);
 })
+
+// 입금액 입력. 입금 버튼 누르면 잔액, 소지금 바꾸기
+btnDeposit.addEventListener('click', (event) => {
+  event.preventDefault()
+})
+
+// 콜라 종류 버튼 누르면 왼쪽 하단에 내역
+
+// 카트로 구매 내역 넘어감
+
+// 총 금액
