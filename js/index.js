557114a2c5d633d6c52b5d38ca0eb8ff7c0626f7
heejiyang <dianoheeji@naver.com>
2022-12-04 03:22:00 +0900

    획득시 계산 js 추가

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ const btnReturn = document.querySelector('.btn-return');
 
 // 획득 내역과 버튼
 const btnGet = document.querySelector('.btn-pocket');
-const cart = document.querySelector('.get-list');
+const cart = document.querySelector('.pocket-list');
 
 // 소지금 획득 음료 목록
 const haveMoney = document.querySelector('.txt-money');
@@ -39,22 +39,55 @@ btnDeposit.addEventListener('click', (event) => {
 
   if (inputCost) {
     if (inputCost <= inhandMoney && inputCost > 0) {
-      inhandMoney.textContent = new Intl.NumberFormat('ko-KR').format(number) + '원';
-      remainMoney.textContent = new Intl.NumberFormat('ko-KR').format(number) + '원';
+      haveMoney.textContent = (inhandMoney - inputCost).toLocaleString() + '원';
+      remain.textContent = (inputCost + (remainMoney ? remainMoney : 0)).toLocaleString() + '원';
     } else {
       alert ('돈이 부족합니다. 입금해주세요.');
     }
+    // 입금이 되면 입금액이 초기화가 되어야한다.
     inpDeposit.value = null;
   }
 })
 
 // 거스름돈 반환
-btnReturn.addEventListener('click', event => {
+btnReturn.addEventListener('click', (event) => {
+  event.preventDefault();
   const inhandMoney = parseInt(haveMoney.textContent.replaceAll(',', ''));
-  const remain = parseInt(haveMoney.textContent.replaceAll(',', ''));
+  const remainMoney = parseInt(remain.textContent.replaceAll(',', ''));
+
+  if (remainMoney) {
+    haveMoney.textContent = (remainMoney + inhandMoney).toLocaleString() + '원';
+    remain.textContent = '0원';
+  }
 })
 
 // 콜라 종류 버튼 누르면 왼쪽 하단에 내역
+// cart: 장바구니 <ul></ul>
+const btnItem = document.querySelectorAll('.item');
+const priceItem = parseInt(document.querySelector('.price-item').textContent);
+
+btnItem.forEach((item) => {
+  // 콜라 종류 불러오는 기능
+  item.addEventListener('click', (event) => {
+    const cartList = document.createElement('li');
+    const remainMoney = parseInt(remain.textContent.replaceAll(',', ''));
+
+    if (remainMoney >= priceItem){
+      cartList.innerHTML = `
+      <button>
+        <img src="./img/${item.dataset.img}" alt="${item.dataset.item}">
+        <p class="list_name">${item.dataset.item}</p>
+        <p class="list_num">1</p>
+      </button>
+      `
+      cart.appendChild(cartList);
+      remain.textContent = (remainMoney - priceItem).toLocaleString() + '원';
+    } else {
+      alert ('돈을 넣으세요~!!')
+    }
+  })
+})
+
 
 // 카트로 구매 내역 넘어감
 
