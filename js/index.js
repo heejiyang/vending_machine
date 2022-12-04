7dcce756162d34e2e9391dd0f3e5d6ba8d860af4
heejiyang <dianoheeji@naver.com>
2022-12-04 23:41:42 +0900

    장바구니 기능 구현

--- js/index.js
+++ js/index.js
@@ -69,26 +69,65 @@ const priceItem = parseInt(document.querySelector('.price-item').textContent);
 btnItem.forEach((item) => {
   // 콜라 종류 불러오는 기능
   item.addEventListener('click', (event) => {
+    const targetEl = event.currentTarget;
     const cartList = document.createElement('li');
     const remainMoney = parseInt(remain.textContent.replaceAll(',', ''));
+    const cartListItem = colaList.querySelectorAll('li');
+    let stack = false;
 
     if (remainMoney >= priceItem){
-      cartList.innerHTML = `
-      <button>
-        <img src="./img/${item.dataset.img}" alt="${item.dataset.item}">
-        <p class="list_name">${item.dataset.item}</p>
-        <p class="list_num">1</p>
-      </button>
-      `
-      cart.appendChild(cartList);
       remain.textContent = (remainMoney - priceItem).toLocaleString() + '원';
-    } else {
-      alert ('돈을 넣으세요~!!')
+
+      // forEach 문을 사용할 경우 반복의 종료가 불가능하다(return, break 작동 안함). 모든 원소를 순환할 필요가 없다면 비효율적.
+      // 클릭한 음료수가 내가 이미 선택한 아이템인지 탐색. 내가 클릭한 상품과 내가 담은 상품이 같을 경우
+      for (const item of cartListItem) {
+        if(item.dataset.item === targetEl.dataset.item) {
+          item.querySelector('.list_num').textContent++;
+          stack = true;
+          break;
+        }
+      };
+
+      // 콜라 처음 선택했을 경우
+      if (!stack){
+        cartList.innerHTML = `
+          <button>
+            <img src="./img/${item.dataset.img}" alt="${item.dataset.item}">
+            <p class="list_name">${item.dataset.item}</p>
+            <p class="list_num">1</p>
+          </button>
+        `
+        cart.appendChild(cartList);
+      }
+      targetEl.dataset.stack--;
+
+      // 상품이 소진되면 품절 표시
+      if (parseInt(targetEl.dataset.stack)){
+        targetEl.classList.add('sold-out');
+        targetEl.disabled = true;
+        const soldOut = document.createElement('em');
+        targetEl.insertAdjacentHTML('afterbegin', '<em class="txt-hide">해당상품은 품절입니다.</em>');
+        // soldOut.classList.add('txt-hide');
+        // soldOut.textContent = '해당상품은 품절입니다.';
+        // targetEl.parentElement.insertBefore(soldOut, targetEl);
+      }
+    } else if (remainMoney < priceItem){
+      alert ('잔액이 부족합니다. 돈을 입금해주세요~!!')
     }
   })
+});
+
+// 획득 버튼 클릭하면 카트로 구매 내역 이동한다.
+btnGet.addEventListener('click', (event) => {
+  let got = false;
+  let totalPrice = 0;
+
+  for (const got of cart.querySelectorAll('li')) {
+    for (const itemGot of getListItem.querySelectorAll('li')) {
+      let gotCount = got.querySelector('.list_num');
+      if ()
+    }
+  }
 })
-
-
-// 카트로 구매 내역 넘어감
 
 // 총 금액
